c1789a832975dde8b1a7ee2b761d0a9d864b06d4
Muhammad Bilal Shaikh <bilal.shaikh@toko.pk>
2026-07-15 14:56:39 +0500

    PUSH

--- myproject/home/migrations/0056_add_gender_guardian_safe.py
+++ myproject/home/migrations/0056_add_gender_guardian_safe.py
@@ -3,15 +3,12 @@ from django.db import migrations
 
 def add_gender_guardian_if_needed(apps, schema_editor):
     from django.db import connection
+    existing = [col.name for col in connection.introspection.get_table_description(connection.cursor(), 'home_admissionapplication')]
     with connection.cursor() as cursor:
-        cursor.execute("""
-            ALTER TABLE home_admissionapplication
-            ADD COLUMN IF NOT EXISTS gender varchar(10) NOT NULL DEFAULT ''
-        """)
-        cursor.execute("""
-            ALTER TABLE home_admissionapplication
-            ADD COLUMN IF NOT EXISTS guardian_name varchar(150) NOT NULL DEFAULT ''
-        """)
+        if 'gender' not in existing:
+            cursor.execute("ALTER TABLE home_admissionapplication ADD COLUMN gender varchar(10) NOT NULL DEFAULT ''")
+        if 'guardian_name' not in existing:
+            cursor.execute("ALTER TABLE home_admissionapplication ADD COLUMN guardian_name varchar(150) NOT NULL DEFAULT ''")
 
 
 class Migration(migrations.Migration):
